9d0d4d3f9ff74797888237ac3f41eddc56f7cdbb
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2026-02-24 15:21:23 +0600

    update message

---
+++ PythonDay2.py
@@ -0,0 +1,2 @@
+print("Hi, I am Evanul Hasan Oualid") 
+# I have to come Day2 again last of this course
---
+++ PythonDay3.py
@@ -0,0 +1,5 @@
+print("Hello I am Evanul Hasan Oualid")
+print("""In Python, a module is a file containing Python code for organization and reuse,
+      while pip is the standard package manager used to install manage external modules 
+      from the Python Package index
+      """)
