25340fb97bcc94c134134fb6befaa23abe6adee9
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2026-02-24 21:55:06 +0600

    Python Variables and Data Types

---
+++ PythonDay5.py
@@ -0,0 +1,25 @@
+# This is the single line comment in Python
+""" This is multiple line
+comment is Python Programming 
+    Language.
+"""
+
+print("Hey I am a good boy")
+print("Hey I am a good boy\nand this viewer is also a good boy/girl")
+
+print("Hey", 6, 7, sep="~", end="009\n") # Here sep="anything" separate all under in one parameter
+''' If i give only end="anything" 
+    it will add next line 
+    to the previous line'''
+    
+print("Evanul Hasan Oualid") 
+
+"""And if add escape characters \n it will
+    make separate next line code with previous 
+    line code """
+
+import sys 
+print(sys.version)  
+
+
+
---
+++ PythonDay6.py
@@ -0,0 +1,34 @@
+# Python Variables and Data Types 
+
+a = 123
+a1 = 9
+print(a)
+print(a+a1)
+print("The type of a is",type(a))
+b = "Evanul"
+print(b)
+print("The type of b is", type(b))
+
+c = True
+print("The type of c is", type(c))
+
+d = None
+print("The type of d is", type(d))
+
+list1 = [8, 3.3, [-4, 6], ["apple", "banana"]]
+print(list1)
+print(type(list1))
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+print(type(tuple1))
+
+dict1 = {"name":"Sakshi", "age":20,"canVote":True}
+print(dict1)
+print(type(dict1))
+
+
+
+
+
+
