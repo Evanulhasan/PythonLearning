10bdc6d9f1b8375b9bda22d1fee32248537e7e81
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2026-02-26 15:56:59 +0600

    Today first commit start day 7

---
+++ PythonDay7.py
@@ -0,0 +1,41 @@
+# Operator in python 
+"""
+# + Addition 
+# - Subtraction 
+# * Multiplication 
+# / Division 
+# % Modulus 
+# ** Exponentiation 
+# / / Floor Division 
+
+"""
+
+# print(5+6)
+# print(15-6)
+# print(15*6)
+# print(15/6)
+# print(15%6)
+# print(2**5)
+# print(15//6)
+
+# Excrcise 1 - Creat a Calculator 
+
+a = 45656 
+b = 37 
+#addition 
+print("The value of", a,"+", b, "is :", a+b)
+print("The value of", a,"-", b, "is :", a-b)
+print("The value of", a,"*", b, "is :", a*b)
+print("The value of", a,"/", b, "is :", a/b)
+print("The value of", a,"%", b, "is :", a%b)
+print("The value of", a,"**", b, "is :", a**b)
+print("The value of", a,"//", b, "is :", a//b)
+
+
+
+
+
+
+
+
+
---
+++ PythonDay8.py
@@ -0,0 +1,25 @@
+# Simple Calculator Using python
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
