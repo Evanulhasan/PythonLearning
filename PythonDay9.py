eabb3bcca3aaa5fd01b5eb301abb36050398dcf0
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2026-03-03 11:27:14 +0600

    Typecasting in python

---
+++ PythonDay9.py
@@ -0,0 +1,46 @@
+# Typecasting in python 
+"""Type casting in Python is
+the process of converting a value
+from one data type to another to 
+ensure compatibility and perform 
+specific operations. Python supports
+two main types of casting: implicit 
+(automatic) and explicit (manual)."""
+
+# Explicit 
+"""Explicit type casting 
+also called type conversion,
+is done manually by the programmer
+using built in functions to convert 
+data to a required type."""
+# a = "1"
+# b = "2"
+# print(a+b)
+# print(int(a)+int(b))
+
+# a = 1 
+# b = 2 
+
+# print(a+b)
+
+# Implicit TypeCasting 
+"""One data type is convertd into other by the 
+Python interpreter itself(automatically)"""
+c = 1.9 
+d = 8 
+print(c+d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
