422ddc7c324488b4c5cef48bad00037b9d92f7b4
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2026-03-03 12:45:24 +0600

    UserInput in Python

---
+++ PythonDay10.py
@@ -0,0 +1,47 @@
+# Taking User Input in Python 
+
+"""User input in Python is handled 
+using the built-in input() function."""
+
+# a = input()
+# print(a)
+
+# a = input("Enter your name: ")
+# print("My name is", a)
+
+x = int(input("Enter First Number: "))
+y = int(input("Enter Second Number: "))
+
+print(f"The addition of {x} + {y} is : {x+y}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
