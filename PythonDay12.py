4f45438a694c9643cc60324c26566f24290fb5c8
Evanulhasanoualid <evanulhasanoualid1410@gmail.com>
2026-03-08 13:15:53 +0600

    String Slicing

---
+++ PythonDay12.py
@@ -0,0 +1,41 @@
+# Strings Slicing and Operations on Strings in Python 
+
+names = "Evanul Hasan Oualid"
+print(len(names))
+print(names[0:5])
+
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is a",len1,"letter word.")
+
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)
+print(fruit[0:-3])
+print(fruit[-1:-3])
+print(fruit[-3:-1])
+
+#Quick Quiz:
+nm = "Evanul"
+print(nm[-4:-2]) # Output will be (an)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
