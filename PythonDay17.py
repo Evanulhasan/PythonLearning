37b70276f1c7d6850d7f327fcfab87d9bf632230
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-15 12:33:12 +0600

    For loop

---
+++ PythonDay17.py
@@ -0,0 +1,39 @@
+# For Loops in python 
+
+
+
+# for i in range(1,101):
+#     print(i)
+
+# name = "Evanul Hasan Oualid"
+# for i in name:
+#     print(i)
+
+colors = ["red","green","blue","yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+
+# Range function in python 
+
+# for k in range(5):
+#     print(k+2)
+
+for k in range(1,11): # it print (n-1) times
+    print(k)    # it print 1 to 10 
+
+print("\n")
+for i in range(2,101,2):
+    print(i)  # it print 2 to 20 even numbers
+
+
+
+
+
+
+
+
+
+
