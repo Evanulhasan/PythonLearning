4bd1203cc3c0803635a4e1a7b1faf72792ea10ec
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-15 15:43:37 +0600

    While loop in Python

---
+++ PythonDay18.py
@@ -0,0 +1,60 @@
+# while loop in python  
+
+for i in range(3):
+    print(i)
+print("\n")
+
+i = 0 
+while i<3:
+    print(i)
+    i = i + 1
+
+print("Done with the loop")
+
+
+# i = int(input("Enter a number: "))
+# while i<=40:
+#    i = int(input("Enter a number: "))
+#    print(i)
+
+# print("Done with the loop")
+
+# pritn
+
+
+count = -5 
+
+while count>0:
+    print(count)
+    count = count - 1
+else:
+    print("I am Inside else block")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
