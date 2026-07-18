b9c36a20d8ce161e9bfc91122fd5ba4e02ddded0
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-18 23:03:50 +0600

    Tuple

---
+++ PythonDay24.py
@@ -0,0 +1,54 @@
+# Tuples In Python                    
+tup = (1,5,7, "Green", True, 6, 8)
+
+print(type(tup), tup)
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[3])
+print(tup[4])
+print("\n")
+print(tup[-1])
+
+if 7 in tup:
+    print("Yes")
+else:
+    print("No")
+
+print(tup[2:5])
+
+tup2 = tup[1:4]
+print(tup2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
