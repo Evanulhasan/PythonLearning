1190ea415714edd7acb8d66d2fc694155276f3b8
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-18 22:08:21 +0600

    List mathod

---
+++ PythonDay23.py
@@ -0,0 +1,73 @@
+# List Methods in Python 
+
+
+l = [11, 2, 46, 6, 7,2,45,46,57,1,3,2]
+print(l)
+# l.append(8) # appeend meand add new value in list
+# print(l)
+# l.sort(reverse=True) # Sorting in list 
+# l.reverse() # Reverse Original list
+# a = l.index(2) # It's give index number of 2
+
+# print(l.index(2)) # It's give index number of 2
+# print(l.count(2)) # It's give how many time 2 exist in list 
+# m = l.copy()
+# m[0] = 0
+# print(m) 
+# l.insert(1,899) # Here index 1 it insert value 899
+m = [900,1000, 1100]
+# l.extend(m) # here add at the end of L all m list value
+k = l+m       # Marge l + m
+print(l)
+print(k)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
