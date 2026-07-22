61b6aa9f0417ed7e16d2e399d8656d63d14a8bc7
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-22 21:04:48 +0600

    Python Docstrings and pip

---
+++ PythonDay29.py
@@ -0,0 +1,43 @@
+# Docstrings in Python  
+# PEP 8 in Python 
+
+
+def square(n):
+    '''Takes in a number n, returns the 
+    square of n'''
+    print(n**2)
+
+square(5)
+print(square.__doc__)
+
+
+# PEP8 = Python Enhancement Proposal
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
