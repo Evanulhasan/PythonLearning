6db067af07338edde3628f81f3983f51f221a126
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-25 18:46:30 +0600

    Set in Python

--- PythonDay30.py
+++ PythonDay30.py
@@ -23,34 +23,34 @@
 # print(factorial(6))
 
 # Fibonacci Calculator  using recartion 
-def febo(n):
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    else:
-        return febo(n-1) + febo(n-2) 
-
-
-a = int(input("Entr the number: "))
-print(febo(a))
-
-
-
-# def get_nth_fibonacci(n):
-#     if n <= 0:
+# def febo(n):
+#     if n==0:
 #         return 0
-#     elif n == 1:
+#     elif n==1:
 #         return 1
-        
-#     a, b = 0, 1
-#     for _ in range(2, n + 1):
-#         a, b = b, a + b
-#     return b
+#     else:
+#         return febo(n-1) + febo(n-2) 
 
-# # Example usage:
-# print(get_nth_fibonacci(40))
-# # Output: 55
+
+# a = int(input("Entr the number: "))
+# print(febo(a))
+
+
+
+def get_nth_fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+        
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+# Example usage:
+print(get_nth_fibonacci(40))
+# Output: 55
 
 
 
---
+++ PythonDay31.py
@@ -0,0 +1,32 @@
+# Sets In Python 
+s ={2, 4, 2, 6}
+print(s)
+
+# Unordered collection of data and set do not contain duplicate items.
+
+info = {"Carla", 10, 19, False, 4.8, 574, 7, 19}
+print(info)
+
+# Empty set
+emp = set()
+print(emp)
+print(type(emp))
+
+# For loop in set 
+for value in info:
+    print(value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
