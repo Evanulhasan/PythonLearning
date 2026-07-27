dbb1c4a6c997baa1e512a023b77187852dafcc31
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-27 16:24:03 +0600

    Raising custom errors in Python

---
+++ PythonDay38.py
@@ -0,0 +1,43 @@
+# Raising Custom errors in Python 
+
+
+a = int(input("Enter any value between 5 and 9: "))
+
+if(a<5 or a>9):
+    raise ValueError("Value should be between 5 and 9")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pythonDay35.py
+++ pythonDay35.py
@@ -37,6 +37,7 @@ print("\n")
 
 for x in range(5):
     print("iteration on {} in for loop". format(x+1))
+    
 else:
     print("else block in loop")
 print("Out of loop")
