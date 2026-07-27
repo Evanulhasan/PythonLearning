c197ce3757c9629d3ee72a785635a2087af15f6e
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-27 12:29:57 +0600

    For loop with else in python

---
+++ pythonDay35.py
@@ -0,0 +1,68 @@
+# For Loop with else in Python 
+
+# Python Allows  the else keyword to be used 
+# with he for and while loops too
+
+
+# for i in []:
+#     print(i)
+    
+# else:
+#     print("Sorry no i")
+    
+# # If for loop run properly
+# whit out break tha At the end else exicute 
+
+
+# for i in range(6):
+#     print(i)
+#     if i == 0:
+#         break
+    
+# else:
+#     print("Sorry no i")
+
+# While loop
+i = 0
+while i<7:
+    print(i)
+    i = i + 1
+    if i == 4:
+        break
+    
+else:
+    print("Sorry no i")
+    
+print("\n")
+
+for x in range(5):
+    print("iteration on {} in for loop". format(x+1))
+else:
+    print("else block in loop")
+print("Out of loop")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
