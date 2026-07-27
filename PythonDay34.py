0f98e54c06e2c9b5d8f97b4eb97500a7148e5835
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-27 11:45:53 +0600

    dictionary in python

---
+++ PythonDay34.py
@@ -0,0 +1,57 @@
+# Dictionary Methods in Python 
+
+ep1 = {122: 45, 123: 89, 567: 69, 670: 69}
+ep2 = {222: 67, 566: 90}
+
+# ep1.update(ep2)
+# print(ep1)  # Update dictionary 
+
+
+
+# ep1.clear() # clear dictionary
+
+# empt = {}
+# print(empt) # {Empty dictionary }
+
+# Remove key:value using pop
+# ep1.pop(122)
+# print(ep1)
+
+# # .popitem() it remove last key:value
+# ep1.popitem()
+# print(ep1)
+
+#it delete dictionary entirly with error
+# del ep1 
+# print(ep1)
+
+# It delete only 122 
+del ep1[122]
+print(ep1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
