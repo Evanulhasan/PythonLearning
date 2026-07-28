536e314cf56241aed56f74fa929bd62c16d28137
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-29 00:01:39 +0600

    Short Hand if else statements in python

---
+++ PythonDay41.py
@@ -0,0 +1,27 @@
+# Short Hand if else statements 
+# Shorthandd syntax can be a  convenient 
+# way to write simple if-else statements,
+
+
+a = 330 
+b = 3303 
+print("A") if a>b else print("=") if a == b else print("B")
+
+
+c = 9 if a>b else 0 
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
