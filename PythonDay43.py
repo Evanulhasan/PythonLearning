886ebdcb782071564964a2f55e6c2150417deafe
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-07-30 00:06:34 +0600

    Python Virtual Environment

---
+++ PythonDay43.py
@@ -0,0 +1,57 @@
+# Virtual Environment in Python 
+
+"""A virtual environment is a tool to isolate specific
+Python environments on a singl emachine, allowingyou to
+work on multiple projects with different dependancies and 
+packages"""
+
+
+# pip freeze > requirements.txt 
+# pip install -r requirements.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
