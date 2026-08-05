8a66f9c9a59584bc1743b22d3ac6c3c8e5c3d665
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-08-06 00:12:32 +0600

    Exercise 4

--- PythonDay47.py
+++ PythonDay47.py
@@ -4,13 +4,21 @@
 st = input("Enter a string: ")
 words = st.split(" ")
 
+coding = input("1 fo coding or 0 for Decoding: ")
+# Condition to check if coding is true or false. If coding is true,
+# then the user wants to encode the string. If coding is false,
+# then the user wants to decode the string.
+coding = True if coding == "1" else False
+
 # If coding is true, Than go to the if block and add "dfs" at the 
 # start and "uyr" at the end of the word. Also, move the first 
 # character of the word to the end of the word.
 
 # If coding is false, Than go to the else block and remove the 
 # first 3 and last 3 characters from the word.
-coding = False
+
+
+print(coding)
 if(coding):
     newword = []
     for word in words:        
