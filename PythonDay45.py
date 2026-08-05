c57d2a8b8807b71cffed2930610fca7699afa192
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-08-05 21:16:05 +0600

    if __name__ =='__main__:'

---
+++ PythonDay45.py
@@ -0,0 +1,40 @@
+# if__name__=="__main__": in python is used to check whether a Python script is being run directly or being imported as a module in another script. When a Python script is run directly, the special variable __name__ is set to "__main__". If the script is imported as a module, __name__ 
+# will be set to the name of the module.
+
+
+import oualid 
+
+oualid.welcome()
+
+# print(oualid.oualid)
+
+# If the module is run directly, the following code will execute
+# If the module is imported, the following code will not execute
+# if__name__=="__main__":
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- oualid.py
+++ oualid.py
@@ -6,11 +6,18 @@
 def welcome():
    
     print("Hello, welcome to the Python learning journey!")
-    
-    
-    
-oualid = "Learning Python is fun and exciting!"
-    
+
+# 
+# This will print the name of the module when imported  
+print(__name__)
+# If the module is run directly, the following code will execute
+# If the module is imported, the following code will not execute
+if __name__ == "__main__":
+    welcome() 
+     
+# oualid = "Learning Python is fun and exciting!"
+
+
     
     
     
