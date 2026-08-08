996297b851a7e7d9c4c6651ce4dbc809286e4ee5
Evanul Hasan Oualid <evanulhasanoualid1410@gmail.com>
2026-08-08 22:00:01 +0600

    'is ' vs '==' Python

---
+++ PythonDay54.py
@@ -0,0 +1,53 @@
+# 'Is'vs '==' in Python 
+
+
+# a = 4 
+# b = '4'
+
+# print(a is b)  # Exact location of object in memory 
+# print(a == b)   # Value
+
+
+# a = [1, 2, 43]
+# b = [1, 2, 43]
+# print(a is b) # False
+# print(a == b) # True 
+
+
+
+# a = 3 
+# b = 3
+# print(a is b)  #True
+# print(a == b)   # True
+
+# a = "oualid"
+# b = "oualid"
+# print(a is b)  #True
+# print(a == b)   # True
+
+
+# a = (3, 4)
+# b = (3, 4)
+# print(a is b)  #True
+# print(a == b)   # True
+
+
+
+a = None
+b = None
+print(a is b)  #True
+print(a == b)   # True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
